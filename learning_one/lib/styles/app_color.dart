fe6a44548f642f93ef840ff3250bc08867a67729
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-02-20 16:02:16 +0000

    add custom color file

---
+++ learning_one/lib/styles/app_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const primary = Color(0xffFBD512);
+  static const fontColor = Color(0xff373737);
+  static const fontColor02 = Color(0xffD8D8D8);
+  static const disableFont = Color(0xffC2C2C2);
+  static const disableBtn = Color(0xff303030);
+  static const backgroundColor = Color(0xff1A2947);
+  static const black = Color(0xffFBD512);
+  static const white = Color(0xffD8D8D8);
+}
