4a57a186a0780141af54ad67d09dbc4021447120
walonCode <mohamedlaminwalonjalloh@gmail.com>
2025-03-16 17:26:04 +0000

    todo app with real api

--- nodejs_server/index.js
+++ nodejs_server/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import {config} from "dotenv"
 import Todo from "./models/todoModel.js"
+import connectDB from "./configs/connectDB.js"
 
 config()
 
@@ -9,6 +10,9 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+connectDB()
+
 
 app.get("/", async(req, res) => {
     try{
@@ -22,3 +26,42 @@ app.get("/", async(req, res) => {
         return res.status(500).json({message: "server error"})
     }
 })
+
+app.post("/", async(req, res) => {
+    try{
+        const todo = await Todo.create(req.body)
+        return res.status(200).json(todo)
+    }catch(error){
+        console.error(error)
+        return res.status(500).json({message: "server error"})
+    }
+})
+
+app.delete('/:id', async(req, res) => {
+    try{
+        const todo = await Todo.findByIdAndDelete(req.params.id)
+        if(!todo){
+            return res.status(404).json({message: "Todo not found"})
+        }
+        return res.status(200).json({message: "Todo deleted", id})
+    }catch(error){
+        console.error(error)
+        return res.status(500).json({message: "server error"})
+    }
+})
+
+app.patch("/:id", async(req,res) => {
+    try{
+        const todo = await Todo.findByIdAndUpdate({_id:req.params.id}, {update : req.body})
+        if(!todo){
+            return res.status(404).json({message: "Todo not found"})
+        }
+        return res.status(200).json({message: "Todo updated", todo})
+    }catch(error){
+        console.error(error)
+        return res.status(500).json({message: "server error"})
+    }
+})
+
+
+app.listen(process.env.PORT, () => console.log(`server is running on port ${process.env.PORT}`))
